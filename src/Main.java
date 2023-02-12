c69cda3d7de0cd7f6248535877800e0ea41a76b6
aau8996 <auad@mail.uc.edu>
2023-02-12 16:04:50 -0500

    Final code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Variables
+        final int MATURE_AGE = 21;
+        int userAge = 65;
+
+        // Prompt from program
+        System.out.println("Please enter you age:");
+        // If userAge >= MATURE_AGE then
+        if(userAge >= MATURE_AGE) {
+            // Output "Please take a wristband."
+            System.out.println("Please take a wristband.");
+        }
+        else{
+            System.out.println();
+        }
+
     }
 }
